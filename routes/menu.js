d4caf6d13b59f9b707ce1b2a3ce508781564a564
yungfloyd <74601945+yungfloyd@users.noreply.github.com>
2020-12-05 14:44:48 +0100

    Add files via upload

--- routes/menu.js
+++ routes/menu.js
@@ -3,17 +3,93 @@ var router = express.Router();
 
 // GET index page.
 router.get('/', function(req, res, next) {
-    res.render('index', {title : "testt", user : req.user});
+    res.render('index');
 });
 
 // GET contact page.
 router.get('/contact', function(req, res, next) {
-    res.render('contact', {user : req.user});
+    res.render('contact');
 });
 
-// GET boeken page.
-router.get('/boeken', function(req, res, next) {
-    res.render('boeken', {user : req.user});
+// GET caddie page.
+router.get('/winkelwagen', function(req, res, next) {
+    res.render('winkelwagen');
 });
+
+// GET shirts page.
+router.get('/shirts', function(req, res, next) {
+    res.render('shirts');
+});
+
+// GET blue shirt page.
+router.get('/shirtBlauw', function(req, res, next) {
+    res.render('shirtBlauw');
+});
+
+// GET red shirt page.
+router.get('/shirtRood', function(req, res, next) {
+    res.render('shirtRood');
+});
+
+// GET black shirt page.
+router.get('/shirtZwart', function(req, res, next) {
+    res.render('shirtZwart');
+});
+
+// GET shorts page.
+router.get('/shorts', function(req, res, next) {
+    res.render('shorts');
+});
+
+// GET blue short page.
+router.get('/shortBlauw', function(req, res, next) {
+    res.render('shortBlauw');
+});
+
+// GET red short page.
+router.get('/shortRood', function(req, res, next) {
+    res.render('shortRood');
+});
+
+// GET black short page.
+router.get('/shortZwart', function(req, res, next) {
+    res.render('shortZwart');
+});
+
+// GET socks page.
+router.get('/sokken', function(req, res, next) {
+    res.render('sokken');
+});
+
+// GET blue socks page.
+router.get('/sokkenBlauw', function(req, res, next) {
+    res.render('sokkenBlauw');
+});
+
+// GET red socks page.
+router.get('/sokkenRood', function(req, res, next) {
+    res.render('sokkenRood');
+});
+
+// GET black socks page.
+router.get('/sokkenZwart', function(req, res, next) {
+    res.render('sokkenZwart');
+});
+
+// GET trainings page.
+router.get('/trainings', function(req, res, next) {
+    res.render('trainings');
+});
+
+// GET blue training page.
+router.get('/trainingBlauw', function(req, res, next) {
+    res.render('trainingBlauw');
+});
+
+// GET black training page.
+router.get('/trainingZwart', function(req, res, next) {
+    res.render('trainingZwart');
+});
+
 
 module.exports = router;
